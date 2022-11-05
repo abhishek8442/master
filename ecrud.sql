873c31db44d12a9b63b7c18fa2755c737c1ba900
naveennayak06 <116938317+naveennayak06@users.noreply.github.com>
2022-11-05 21:16:55 +0530

    Update ecrud.sql

--- ecrud.sql
+++ ecrud.sql
@@ -5,3 +5,5 @@ salary int, Email varchar(150),password varchar(8),joining_date date , contact_n
 gender enum('m','f') , attendance int default(0),leave_count int,status varchar(10) default'non-active');
 create table notice(rel_time date,notice text);
 Create table complain(complain_id int auto_increment primary key, employee_id int ,complain_text text);
+alter table employeemanagement.employee_table modify contact_no int;
+alter table employeemanagement.employee_table modify status enum('non-active','active') default'non-active';
